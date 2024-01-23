7897510988c1c89e630ea9132a9deea00421f821
Lucas Dias <lucas-dias@hotmail.com>
2024-01-23 11:04:03 -0300

    ajuste layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { fakeBackendProvider } from './core/interceptors/fake-backend.interceptor';
 import { HttpClientModule } from '@angular/common/http';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     HttpClientModule
   ],
-  providers: [fakeBackendProvider],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [fakeBackendProvider,{provide: LocationStrategy, useClass: PathLocationStrategy}],
 })
 export class AppModule { }
